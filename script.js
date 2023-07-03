a5bda620a84b19e1fb0c8a2d8844c3d0a1c7cfbc
Renato Gomes <renatogomes.0405@gmail.com>
2023-07-03 18:39:10 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+function off() {
+    let ver = document.getElementById('balao3')
+    let no = document.getElementById('balao2')
+
+    ver.style.display = 'block'
+    no.style.display = 'none'
+    
+}
